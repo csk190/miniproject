c31dc745dfadf169792147c316ba702a3fc8a025
csk190 <csk190@gmail.com>
2026-04-24 14:33:52 +0900

    Update image URLs in Home.py

--- Home.py
+++ Home.py
@@ -1,11 +1,11 @@
 mport streamlit as st
 
-# url = "https://github.com/MK316/Applied-linguistics/blob/main/images/AL-bg1.png"
+# url = "https://github.com/csk190/miniproject/blob/main/images/Gemini_Generated_Image_q5723kq5723kq572.png"
 
 st.markdown("#### Spring 2026")
 st.caption("This page is continuously updated.")
 
-IMAGE_URL = "https://raw.githubusercontent.com/MK316/Applied-linguistics/main/images/AL-bg1.png"
+IMAGE_URL = "https://github.com/csk190/miniproject/blob/main/images/Gemini_Generated_Image_q5723kq5723kq572.png"
 
 st.image(
     IMAGE_URL,
