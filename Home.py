888b801c0d2c31d7e4f6de8e7ded751b5ce42482
csk190 <csk190@gmail.com>
2026-04-24 14:28:31 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,1 +1,13 @@
+mport streamlit as st
 
+# url = "https://github.com/MK316/Applied-linguistics/blob/main/images/AL-bg1.png"
+
+st.markdown("#### Spring 2026")
+st.caption("This page is continuously updated.")
+
+IMAGE_URL = "https://raw.githubusercontent.com/MK316/Applied-linguistics/main/images/AL-bg1.png"
+
+st.image(
+    IMAGE_URL,
+    use_container_width=True
+)
